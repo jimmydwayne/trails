14767a4d036b3f024bd28da60e3dbcbd6a044c91
Dwayne <jimmydwayne@gmail.com>
2018-11-17 23:35:23 -0500

    add trails to array

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cheerio = require("cheerio");
 var options = {
   headers: { "user-agent": "node.js" }
 };
-
+// var data = [];
 //finds and returns the text of the first li in the slider
 var html = request("http://capefearsorba.org/", options, function(
   err,
@@ -19,41 +19,13 @@ var html = request("http://capefearsorba.org/", options, function(
     let list = $("#edn-effect-slider");
     var list2 = list
       .find("li")
-      .first()
-      .text();
-    var first = list2.toString().trim();
-    // console.log(first);
+      .text()
+      .replace(/(\r\n|\n|\r)/gm, "");
+    var data = list2.split("  ");
+    var filtered = data.filter(function(el) {
+      return el != "";
+    });
+    var trails = filtered;
+    console.log(trails);
   }
 });
-
-// let scoreArr = (titleArr = []);
-// var html = request("http://capefearsorba.org/", options, function(
-//   err,
-//   resp,
-//   html
-// ) {
-//   if (!err) {
-//     const $ = cheerio.load(html);
-//     // console.log(html);
-//     var slider = $("#edn-effect-slider").children();
-//     var list = slider.find("li").text();
-//     for (var i = 0; i < list.length; i++) {
-//       var trails = list[i];
-//       scoreArr.push()
-//       // alert(arrValue);
-//     }
-//     console.log(trails);
-//   }
-// });
-
-//Scrape karma scores
-let scoreArr = (titleArr = []);
-request("http://capefearsorba.org/", (err, res, body) => {
-  const $ = cheerio.load(body);
-
-  $("#edn-effect-slider").attr("li", (i, val) => {
-    scoreArr.push(val);
-  });
-});
-
-console.log(scoreArr.toString().trim());
